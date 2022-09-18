085d3d7e6efc6436b5b7f81e36c63103026db218
Alexander Ryzhov <aryzhov84@gmail.com>
2022-09-18 21:44:38 +0300

    ввел метод с описанием формирования нового массива

--- FinalTask/Program.cs
+++ FinalTask/Program.cs
@@ -13,3 +13,15 @@ void PrintArray(string[] arr)
     }
     Console.WriteLine();
 }
+void Formation(string[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        string element = arr[i];
+        if (element.Length <= 3)
+        {
+            arr[i] = element;
+    }
+        else arr[i] = string.Empty;
+    }
+}
